a93d6c81c7ff6e17b0ef11f605ef834b9c1de1c3
Gabriel Siwa <gabrielirasiwa.8@gmail.com>
2025-12-06 01:46:35 -0700

    [FIX] Deploying in vercel instead!

--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
 from flask import Flask, send_file
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='../static', static_url_path='/static')
 
 @app.route('/')
 def index():
-    return send_file('index.html')
+    return send_file('../index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
